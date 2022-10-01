1392fbb9fcc55edd58d2a87c0f84f831a26e3b68
Igor <kugimn@list.ru>
2022-10-02 02:21:46 +0300

    All +

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int SumOddIndexes(int[] incomingArray)
+{
+    int sum = 0;
+    for (int i = 1; i < incomingArray.Length; i+=2)
+    {
+        if (i % 2 != 0)
+        {
+            sum += incomingArray[i];
+        }
+    }
+    return sum;
+}
+
+
+int[] currentArray = getRandomArray(10, 1, 20);
+printArray(currentArray);
+
+int count = SumOddIndexes(currentArray);
+Console.WriteLine($"Cумма элементов, стоящих на нечетных позициях в массиве {count}");
+
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+double DifferenceMaxMinInArray(int[] currentArray)
+{
+    int maxElement = currentArray[0];
+    int minElement = currentArray[0];
+    for (int i = 1; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] < minElement)
+        {
+            minElement = currentArray[i];
+        }
+        if (currentArray[i] > maxElement)
+        {
+            maxElement = currentArray[i];
+        }
+    }
+    return maxElement - minElement;
+}
+
+Console.Write("Укажите количество элементов в массиве: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] currentArray = getRandomArray(length, 1, 50);
+printArray(currentArray);
+
+double DifferenceFinal = DifferenceMaxMinInArray(currentArray);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {DifferenceFinal}");
+
